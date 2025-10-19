3a4881b96ecadfdfbdb186857d8c5b31d0beab2b
Samar Khan <procanva900@gmail.com>
2025-10-19 19:17:18 +0500

    Update student.js

--- src/config/student.js
+++ src/config/student.js
@@ -1,4 +1,2 @@
-// Edit these two fields with your details before you start.
-// They will be shown at the very top of the screen.
-export const STUDENT_NAME = "Your Name";
-export const REG_NO = "REG-000000";
+export const STUDENT_NAME = "Samar Safdar Khan";
+export const REG_NO = "SP23-BSE-043";
