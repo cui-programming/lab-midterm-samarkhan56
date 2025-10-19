791659121b2f42d189848a4ee75e5a9ce440409b
Samar Khan <procanva900@gmail.com>
2025-10-19 19:18:36 +0500

    Update index.js

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,6 @@
-// Barrel exports for UI components.
-// Intentionally missing the export for './Text' (students must add it).
+import { View as RNView } from 'react-native';
+export const View = RNView;
+
 export { default as Button } from './Button';
 export { default as TextInput } from './TextInput';
-// export { default as Text } from './Text'; // <-- students will uncomment/add this
+export { default as Text } from './Text';
